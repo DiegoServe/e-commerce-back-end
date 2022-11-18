da15745f605f3f08363c7d12d1f5c490f144d4b0
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-11-17 19:45:58 -0500

    tags belongto many

--- models/index.js
+++ models/index.js
@@ -22,6 +22,11 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'tag_id',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
